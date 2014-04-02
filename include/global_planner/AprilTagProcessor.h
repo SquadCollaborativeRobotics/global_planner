4ddc5003bdf0353e204ccd9c00592ee0cbbf8560
Shawn Hanna <shawnkarlhanna@gmail.com>
2014-04-02 14:37:01 -0400

    Added remaining localization code

--- include/global_planner/AprilTagProcessor.h
+++ include/global_planner/AprilTagProcessor.h
@@ -14,11 +14,13 @@
 #include <april_tags/AprilTagList.h>
 #include <april_tags/AprilTag.h>
 #include <tf/transform_listener.h>
+#include <global_planner/GoalMsg.h>
 
 #include <boost/shared_ptr.hpp>
 
-#define STOP_LIN_THRESHOLD 0.01
-#define STOP_ANG_THRESHOLD 0.01
+#define STOP_LIN_THRESHOLD 0.02 //Meters/sec
+#define STOP_ANG_THRESHOLD 0.01 // Radians/sec
+#define UPDATE_RANGE_THRESHOLD 2.0 //meters
 
 class AprilTagProcessor
 {
@@ -41,34 +43,51 @@ public:
      */
     bool ShouldPause();
     bool ShouldResume();
+    bool SetShouldPause(bool shouldPause);
 
 private:
     void cb_aprilTags(const april_tags::AprilTagList::ConstPtr &msg);
 
-
     bool UpdatePose();
+    bool FindGoals();
 
     ros::Time LastSeenTime(int tagID);
+    ros::Time LastGoalSendTime(int tagID);
     void GetLandmarks(std::vector<int> ret);
     void GetGoals(std::vector<int> ret);
 
 
     AprilTagProcessor::TAG_TYPE GetType(int tagID);
     bool IsLandmarkVisible();
+    bool IsStopped();
+
     double GetDistance(tf::StampedTransform &tf);
+    double GetDistance(geometry_msgs::PoseStamped &pose);
     bool PosesDiffer(geometry_msgs::PoseWithCovariance& poseWithCovariance1, geometry_msgs::PoseWithCovariance& poseWithCovariance2);
     void PrintTransform(tf::StampedTransform& transform);
 
-    bool GetMapTransform(std::string frame_name, tf::StampedTransform &tf);
-    bool GetTransform(std::string frame_name1, std::string frame_name2, tf::StampedTransform &tf);
+    bool GetMapTransform(std::string frame_name, tf::StampedTransform &tf, ros::Time=ros::Time(0));
+    bool GetTransform(std::string frame_name1, std::string frame_name2, tf::StampedTransform &tf, ros::Time=ros::Time(0));
 
     std::string GetTagFrameName(int tagID);
     std::string GetLandmarkFrameName(int tagID);
     int GetIDFromFrameName(std::string frame_name);
 
+    /***************************************************************
+     * MEMBER VARIABLES
+     **************************************************************/
+
+    std::string m_cameraFrame;
+    std::string m_robotBaseFrame;
+
     // A map that keeps track of all tags seen and the time it was last seen (using the pose stamped time)
     std::map<int, geometry_msgs::PoseStamped> m_pose;
+
+    // Map that keeps track of the tag's type
     std::map<int, AprilTagProcessor::TAG_TYPE> m_goalTypeMap;
+
+    // Map that keeps track of when the last time this goal was published
+    std::map<int, ros::Time> m_goalSendTime;
     ros::Time m_lastImageTime;
     ros::Time m_lastLocalizeTime;
 
@@ -76,14 +95,26 @@ private:
     int m_robotID;
     bool m_shouldPause;
 
-    // AMCL pose
+    bool m_seesGoal;
+    bool m_seesLandmark;
+
+    /***************************************************************
+     * AMCL pose
+     **************************************************************/
+    //Subscribes to the amcl pose message
     ros::Subscriber m_amclPoseSub;
     geometry_msgs::PoseWithCovarianceStamped m_amclPose;
+    // Publisher that publishes a pose for visualization purposes to the "/new_pose"
+    ros::Publisher m_newPosePub;
+    // Publisher that reinitializes the pose of the robot (relocalizes)
+    ros::Publisher m_newInitialPosePub;
+
+    // Publisher that sends out an april tag that is a possible goal node?
+    ros::Publisher m_goalPub;
 
     // current odometry of the robot. mainly used for velocity
     ros::Subscriber m_odomSub;
     nav_msgs::Odometry m_odom;
-    bool IsStopped();
 
     //Subscribe to April Tag messages from the robot
     ros::Subscriber m_tagSub;
--- src/AprilTagProcessor.cpp
+++ src/AprilTagProcessor.cpp
@@ -13,7 +13,11 @@
  ***********************************************************************/
 AprilTagProcessor::AprilTagProcessor():
 m_robotID(-1),
-m_shouldPause(false)
+m_shouldPause(false),
+m_seesGoal(false),
+m_seesLandmark(false),
+m_lastImageTime(ros::Time(0)),
+m_lastLocalizeTime(ros::Time(0))
 {
 }
 
@@ -38,6 +42,11 @@ bool AprilTagProcessor::Init(ros::NodeHandle *nh, int robotID)
     m_tagSub = nh->subscribe("april_tags", 100, &AprilTagProcessor::cb_aprilTags, this);
     m_robotID = robotID;
     m_tfListener.reset( new tf::TransformListener(*nh) );
+
+    m_goalTypeMap[3] = AprilTagProcessor::LANDMARK;
+    m_goalTypeMap[5] = AprilTagProcessor::LANDMARK;
+    m_goalTypeMap[8] = AprilTagProcessor::GOAL;
+    m_goalTypeMap[6] = AprilTagProcessor::GOAL;
 }
 
 
@@ -48,6 +57,13 @@ bool AprilTagProcessor::Init(ros::NodeHandle *nh, int robotID)
  ***********************************************************************/
 bool AprilTagProcessor::Execute()
 {
+    if (IsStopped())
+    {
+        FindGoals();
+        UpdatePose();
+        return true;
+    }
+    return false;
 }
 
 
@@ -71,19 +87,26 @@ bool AprilTagProcessor::ShouldPause()
  ***********************************************************************/
 bool AprilTagProcessor::ShouldResume()
 {
+    //Wait at least a second before allowing the robot to resume
+    if (ros::Time::now() - m_lastLocalizeTime > ros::Duration(1.0))
+        return false;
     return !m_shouldPause;
 }
 
 
 /***********************************************************************
  *  Method: AprilTagProcessor::UpdatePose
- *  Params: int tagID
+ *  Params:
  * Returns: bool
  * Effects: Update the pose using the tag specified in the parameter
  ***********************************************************************/
 bool AprilTagProcessor::UpdatePose()
 {
-    bool retVal = false;
+    if (IsStopped() == false)
+    {
+        ROS_ERROR_STREAM_THROTTLE(0.5, "ERROR: Robot is not yet stopped");
+        return false;
+    }
 
     std::vector<int> landmarks;
     GetLandmarks(landmarks);
@@ -95,7 +118,7 @@ bool AprilTagProcessor::UpdatePose()
     {
         int tagID = landmarks[i];
         ros::Time tagSeenTime = LastSeenTime(tagID);
-        if (m_lastImageTime == m_lastImageTime)
+        if (tagSeenTime == m_lastImageTime)
         {
             // closestLandmark = dist;
             bestLandmarkId = tagID;
@@ -104,7 +127,7 @@ bool AprilTagProcessor::UpdatePose()
 
     if (bestLandmarkId < 0)
     {
-        ROS_ERROR_STREAM("No images are in camera frame");
+        ROS_ERROR_STREAM("No landmarks are in camera frame");
         return false;
     }
 
@@ -114,14 +137,171 @@ bool AprilTagProcessor::UpdatePose()
 
     std::string landmark_frame;
     std::string april_frame;
-    if (m_tfListener->canTransform(landmark_frame, april_frame, ros::Time(0)))
-    {
 
+    tf::StampedTransform mapLandmarkTF;
+    if (GetMapTransform(landmark_frame, mapLandmarkTF))
+    {
+        tf::StampedTransform tagCameraTF;
+
+        std::string tagFrameString = GetTagFrameName(bestLandmarkId);
+        if (GetTransform(tagFrameString, m_cameraFrame, tagCameraTF))
+        {
+            tf::StampedTransform cameraBaseTF;
+            if (GetTransform(m_cameraFrame, m_robotBaseFrame, cameraBaseTF))
+            {
+                // New amcl pose, in tf form
+                tf::Transform newAMCLPoseTF = mapLandmarkTF * tagCameraTF * cameraBaseTF;
+
+                // Convert to a geometry message, which is what AMCL accepts
+                // New AMCL pose, in message form
+                geometry_msgs::Transform newAMCLTFMsg;
+                tf::transformTFToMsg(newAMCLPoseTF, newAMCLTFMsg);
+
+                //AMCL takes the pose as a PoseWithCovarianceStamped. Build the poseWithCovariance first
+                geometry_msgs::PoseWithCovariance poseWithCovariance;
+                poseWithCovariance.pose.position.x = newAMCLTFMsg.translation.x;
+                poseWithCovariance.pose.position.y = newAMCLTFMsg.translation.y;
+                poseWithCovariance.pose.position.z = newAMCLTFMsg.translation.z;
+                poseWithCovariance.pose.orientation = newAMCLTFMsg.rotation;
+
+                // create the covariance array (the values are based on what rviz sends)
+                // TODO: change based on confidence (distance to tag, speed of turning, etc)
+                // row order covariance.... first row (0-5) = covariance from x, (6-12) = cov. from y, etc.
+                // in this order: (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+                boost::array<float, 36> covariance = {
+                    0.03, 0.0, 0.0, 0.0, 0.0, 0.0, // there is some variance in x due to moving in x
+                    0.0, 0.03,0.0, 0.0, 0.0, 0.0,  // there is some variance in y due to moving in y
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,  // No z motion occurs... no variance
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,  // no x rotation occurs
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,  // no y rotation ccurs
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.05  // z rotation is fairly uncertain???
+                };
+                poseWithCovariance.covariance = covariance;
+
+                // Create a "stamped" message
+                geometry_msgs::PoseWithCovarianceStamped newRobotPoseMsg;
+                newRobotPoseMsg.header.frame_id = "map";
+                newRobotPoseMsg.header.stamp = tagCameraTF.stamp_;
+
+                newRobotPoseMsg.pose = poseWithCovariance;
+
+                //Use for display purposes...
+                geometry_msgs::PoseStamped poseStamped;
+                poseStamped.header.frame_id = "map";
+                poseStamped.header.stamp = tagCameraTF.stamp_;
+                poseStamped.pose = poseWithCovariance.pose;
+
+
+                if (PosesDiffer(m_amclPose.pose, newRobotPoseMsg.pose))
+                {
+                    m_newPosePub.publish(poseStamped);
+                    m_newInitialPosePub.publish(newRobotPoseMsg);
+                    m_lastLocalizeTime = ros::Time::now();
+                    m_shouldPause = false;
+
+                    //wait for amcl to reinitialize
+                    ROS_INFO_STREAM("Wait for AMCL to reinitialize");
+                }
+                else
+                {
+                    ROS_WARN_STREAM("Poses do not differ enough to need april tag localization");
+                    m_shouldPause = false;
+                }
+                return true;
+            }
+            else
+            {
+                ROS_ERROR("Cannot transform from camera to robot base");
+                return false;
+            }
+        }
+        else
+        {
+            ROS_ERROR("Cannot transform from tag to camera");
+            return false;
+        }
+    }
+    else
+    {
+        ROS_ERROR("Cannot transform from map to landmark");
+        return false;
     }
 
-    if (retVal == true)
-        m_lastLocalizeTime = ros::Time::now();
-    return retVal;
+    return false;
+}
+
+
+/***********************************************************************
+ *  Method: AprilTagProcessor::FindGoals
+ *  Params:
+ * Returns: bool
+ * Effects:
+ ***********************************************************************/
+bool AprilTagProcessor::FindGoals()
+{
+    if (IsStopped() == false)
+    {
+        ROS_ERROR_STREAM_THROTTLE(0.5, "ERROR: Robot is not yet stopped");
+        return false;
+    }
+
+    std::vector<int> goals;
+    GetGoals(goals);
+    // double closestLandmark = 9999999;
+
+    //Pick best tag to use for localization (assuming there are several options)
+    for (int i = 0; i < goals.size(); ++i)
+    {
+        int tagID = goals[i];
+        ros::Time tagSeenTime = LastSeenTime(tagID);
+
+        if (tagSeenTime == m_lastImageTime)
+        {
+            std::string goal_frame = GetTagFrameName(tagID);
+
+            tf::StampedTransform transform;
+            if (GetTransform("map", goal_frame, transform))
+            {
+                global_planner::GoalMsg can;
+                geometry_msgs::PoseStamped ps;
+                tf::Quaternion quat;
+
+                quat = transform.getRotation();
+
+                // Get pose in map frame
+                ps.header.frame_id = "map";
+
+                //Set garbage pose based on the transform
+                ps.pose.position.x = transform.getOrigin().x();
+                ps.pose.position.y = transform.getOrigin().y();
+                ps.pose.position.z = transform.getOrigin().z();
+
+                //Set garbage rotation from the transform
+                geometry_msgs::Quaternion quatMsg;
+                quatMsg.x=quat.x();
+                quatMsg.y=quat.y();
+                quatMsg.z=quat.z();
+                quatMsg.w=quat.w();
+                ps.pose.orientation = quatMsg;
+
+                //Finish creating message & publish
+                can.pose = ps.pose;
+                //TODO: make more correct
+                can.id = 6;
+
+                m_goalPub.publish(can);
+                m_goalSendTime[tagID] = ros::Time::now();
+
+                ROS_ERROR("Sent goal pose");
+            }
+        }
+    }
+
+    //Update the pose of the robot
+    //
+    //First, get the frame names: (one for landmark, one for april tag)
+
+    return true;
 }
 
 
@@ -142,6 +322,7 @@ void AprilTagProcessor::GetLandmarks(std::vector<int> vec)
         }
     }
 }
+
 
 /***********************************************************************
  *  Method: AprilTagProcessor::GetGoals
@@ -170,7 +351,11 @@ void AprilTagProcessor::GetGoals(std::vector<int> vec)
  ***********************************************************************/
 ros::Time AprilTagProcessor::LastSeenTime(int tagID)
 {
-    return m_pose[tagID].header.stamp;
+    if (m_pose.find(tagID) != m_pose.end())
+    {
+        return m_pose[tagID].header.stamp;
+    }
+    return ros::Time(0);
 }
 
 
@@ -207,27 +392,62 @@ void AprilTagProcessor::cb_aprilTags(const april_tags::AprilTagList::ConstPtr &m
     else
     {
         ROS_INFO_STREAM(msg->poses.size()<<" tags seen in the robot's camera this time");
-        for (int i = 0; i < numTags; ++i)
-        {
-            //m_timeSeen[msg->tag_ids[i]] = msg->poses[i].header.stamp;
-        }
     }
 
     for (int i = 0; i < numTags; ++i)
     {
         m_pose[msg->tag_ids[i]] = msg->poses[i];
 
-        // m_pose[msg->tag_ids[i]] = CameraPoseToGlobalPose(msg->poses[i]);
+        AprilTagProcessor::TAG_TYPE type = GetType(msg->tag_ids[i]);
+        if (type == AprilTagProcessor::GOAL)
+        {
+            m_seesGoal = true;
+        }
+        else if(type == AprilTagProcessor::LANDMARK)
+        {
 
-        if (m_pose[msg->tag_ids[i]].header.stamp - m_lastLocalizeTime > ros::Duration(9.0) ) {
-            //The robot hasn't updated in a while...
-            m_shouldPause = true;
+        }
+        else
+        {
+            ROS_ERROR("ERROR: Unkown tag type detected");
+        }
+
+        if (type != AprilTagProcessor::UNKNOWN)
+        {
+            // Don't worry about pausing if the robot is currently paused
+            if (m_shouldPause == false)
+            {
+                if (type == AprilTagProcessor::LANDMARK)
+                {
+                    // Only need to update if it's been a while since the robot localized
+                    if (m_pose[msg->tag_ids[i]].header.stamp - m_lastLocalizeTime > ros::Duration(9.0) ) {
+                        //Check if the tag is closer than the threshold distance
+                        if (GetDistance(m_pose[msg->tag_ids[i]]) < UPDATE_RANGE_THRESHOLD)
+                        {
+                            m_shouldPause = true;
+                            m_seesLandmark = true;
+                        }
+                    }
+                }
+                else if (type == AprilTagProcessor::GOAL)
+                {
+                    // Only attempt to send the goal again if it's been a while since last sending it
+                    if (m_pose[msg->tag_ids[i]].header.stamp - LastGoalSendTime(msg->tag_ids[i]) > ros::Duration(9.0) ) {
+                        //The robot hasn't updated in a while...
+
+                        //Check if the tag is closer than the threshold distance
+                        if (GetDistance(m_pose[msg->tag_ids[i]]) < UPDATE_RANGE_THRESHOLD)
+                        {
+                            m_shouldPause = true;
+                            m_seesGoal = true;
+                        }
+                    }
+                }
+            }
         }
     }
 
     m_lastImageTime = msg->poses[0].header.stamp;
-
-    Execute();
 }
 
 
@@ -290,13 +510,8 @@ bool AprilTagProcessor::IsLandmarkVisible()
     //Check through the landmarks to see if there are any currently in view
     for (int i = 0; i < landmarks.size(); ++i)
     {
-        if (GetType(i) != LANDMARK)
-        {
-            // ROS_ERROR_STREAM("ERROR: upadting pose with tag ID ("<<i<<") failed. This tag is not a landmark");
-            continue;
-        }
-
-        if (LastSeenTime(i) != m_lastImageTime)
+        ros::Time lastSeen = LastSeenTime(i);
+        if (lastSeen != ros::Time(0) && lastSeen != m_lastImageTime)
         {
             // ROS_ERROR_STREAM("ERROR: Tag ID ("<<i<<") is not in view of the camera right now");
             continue;
@@ -400,7 +615,7 @@ int AprilTagProcessor::GetIDFromFrameName(std::string frame_name)
  * Effects: gives you the location of a frame in map coordinates,
  * if possible
  ***********************************************************************/
-bool AprilTagProcessor::GetMapTransform(std::string frame_name, tf::StampedTransform &tf)
+bool AprilTagProcessor::GetMapTransform(std::string frame_name, tf::StampedTransform &tf, ros::Time time)
 {
     if (GetTransform("map", frame_name.c_str(), tf))
         return true;
@@ -415,7 +630,55 @@ bool AprilTagProcessor::GetMapTransform(std::string frame_name, tf::StampedTrans
  * Returns: bool
  * Effects:
  ***********************************************************************/
-bool AprilTagProcessor::GetTransform(std::string frame_name1, std::string frame_name2, tf::StampedTransform &tf)
+bool AprilTagProcessor::GetTransform(std::string frame_name1, std::string frame_name2, tf::StampedTransform &tf, ros::Time time)
 {
+    if (m_tfListener->canTransform(frame_name1, frame_name2, time))
+    {
+        m_tfListener->lookupTransform(frame_name1, frame_name2, time, tf);
+    }
+    else
+    {
+        return false;
+    }
 }
+
+/***********************************************************************
+ *  Method: AprilTagProcessor::SetShouldPause
+ *  Params: bool shouldPause
+ * Returns: bool
+ * Effects:
+ ***********************************************************************/
+bool AprilTagProcessor::SetShouldPause(bool shouldPause)
+{
+    m_shouldPause = shouldPause;
+    return m_shouldPause;
+}
+
+
+/***********************************************************************
+ *  Method: AprilTagProcessor::GetDistance
+ *  Params: geometry_msgs::PoseStamped &pose
+ * Returns: double
+ * Effects:
+ ***********************************************************************/
+double AprilTagProcessor::GetDistance(geometry_msgs::PoseStamped &pose)
+{
+    return sqrt(pose.pose.position.x * pose.pose.position.x + pose.pose.position.y*pose.pose.position.y);
+}
+
+/***********************************************************************
+ *  Method: AprilTagProcessor::LastGoalSendTime
+ *  Params: int tagID
+ * Returns: ros::Time
+ * Effects:
+ ***********************************************************************/
+ros::Time AprilTagProcessor::LastGoalSendTime(int tagID)
+{
+    if (m_goalSendTime.find(tagID) != m_goalSendTime.end())
+    {
+        return m_goalSendTime[tagID];
+    }
+    return ros::Time(0);
+}
+
 
